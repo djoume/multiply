4ce7d134db129f839d5a5932323744f91e5c255d
Djoume Salvetti <djoume.salvetti@shopify.com>
2019-03-03 16:14:51 -0500

    Updates

---
+++ precache-manifest.f5d255cd33b9f9157f243e1b4c822926.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "780cfa60f00b20c6c846",
+    "url": "/multiply/static/css/main.5bf98feb.chunk.css"
+  },
+  {
+    "revision": "780cfa60f00b20c6c846",
+    "url": "/multiply/static/js/main.780cfa60.chunk.js"
+  },
+  {
+    "revision": "9367e3147e602dbf7ce9",
+    "url": "/multiply/static/js/runtime~main.9367e314.js"
+  },
+  {
+    "revision": "015ea03cb7b11a05a1c5",
+    "url": "/multiply/static/js/2.015ea03c.chunk.js"
+  },
+  {
+    "revision": "34ecf5b5480d039911076c7ef00523f0",
+    "url": "/multiply/index.html"
+  }
+];
